2484fd6ac7fd81cff86b6353e38a43e60207a89a
Juliana Castillo <96964513+jlianacastillo@users.noreply.github.com>
2022-02-18 15:09:35 -0500

    Create Condicionales.js

---
+++ Condicionales.js
@@ -0,0 +1,17 @@
+//Condicionales-reglas
+
+if (//tiene que ser verdad) {
+//tarea de la condicion 
+}
+
+if (true) {
+  console.log("Hola");
+  }
+  
+//else default
+
+if (false) {
+  console.log("Hola);
+  } else { 
+    console.log("soy falso");
+    }
