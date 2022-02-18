79ff15e90650d28b06829eefff045827707d5246
Juliana Castillo <96964513+jlianacastillo@users.noreply.github.com>
2022-02-18 15:13:48 -0500

    Votar Ejercicio PLATZI

---
+++ Votar.js
@@ -0,0 +1,9 @@
+var edad = 18;
+
+if(edad === 18) {
+  console.log("Tu puedes votar, sera tu primera vez");
+} else if (edad  > 18){
+  console.log("Puedes votar de nuevo, pasa a la urna");
+} else {
+  console.log("Aun no puedes votar, espera tener 18");
+}
